234c73064bd045f8b74360d08b8784e7522c9c1a
Knux <knuxfan@live.co.uk>
2022-10-27 01:24:25 +0100

    Removed that list of headers
    Why have it if 99% of it is currently "NOT SUPPORTED"

--- JackboxLibCmd/Program.cs
+++ JackboxLibCmd/Program.cs
@@ -24,33 +24,14 @@ namespace JackboxLibCmd
             // Split text file.
             string[] text = File.ReadAllLines(args[0]);
 
-            // TODO: Finish setting up all this.
             // Find the file type.
             switch (text[0])
             {
-                case "[JPP1DrawfulPrompts]": throw new Exception("Drawful prompt importing not yet supported.");
-                case "[JPP1FibbageQuestions]": throw new Exception("Fibbage question importing not yet supported.");
-                case "[JPP1LieSwatterQuestions]": throw new Exception("Lie Swatter question importing not yet supported.");
-                case "[JPP1WordSpudWords]": throw new Exception("Word Spud starter word importing not yet supported.");
-                case "[JPP1YDKJEpisode]": throw new Exception("You Don't Know Jack 2015 Episode creation not yet supported.");
-
-                case "[JPP2BidiotsPrompts]": throw new Exception("Bidiots prompt and image importing not yet supported.");
-                case "[JPP2EarwaxPrompts]": throw new Exception("Earwax prompt importing not yet supported.");
-                case "[JPP2EarwaxAudio]": throw new Exception("Earwax sound importing not yet supported.");
-                case "[JPP2FibbageQuestions]": throw new Exception("Fibbage 2 question importing not yet supported.");
-                case "[JPP2QuiplashQuestions]": throw new Exception("Quiplash 2 question importing not yet supported.");
-
-                case "[JPP3TKOSuggestions]": throw new Exception("Tee-KO suggestion importing not yet supported.");
-                case "[JPP3FakingItWriting]": throw new Exception("Faking It writing task importing not yet supported.");
-                case "[JPP3FakingItAction]": throw new Exception("Faking It action task importing not yet supported.");
-                case "[JPP3GuesspionageQuestion]": throw new Exception("Guesspionage normal question importing not yet supported.");
-                case "[JPP3GuesspionageQuestionFinal]": throw new Exception("Guesspionage final round question importing not yet supported.");
-
                 case "[JPP9QuixortTeams]": Process(args, text, typeof(JackboxLib.JPP9.Lineup.Teams)); break;
                 case "[JBB9QuixortTutorial]": Process(args, text, typeof(JackboxLib.JPP9.Lineup.Tutorial));  break;
                 case "[JBB9QuixortPrompts]": Process(args, text, typeof(JackboxLib.JPP9.Lineup.Sequence), true);  break;
 
-                default: throw new Exception("No data found to import.");
+                default: throw new Exception("No supported data found to import.");
             }
         }
 
